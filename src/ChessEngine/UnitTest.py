575faf9cbc23759b97fc2b059ef5e2b7229142ca
James Likier <jlikier@gmail.com>
2010-09-03 13:49:46 -0600

    added some commenting to organize the test cases

--- src/ChessEngine/UnitTest.py
+++ src/ChessEngine/UnitTest.py
@@ -2,12 +2,16 @@ import unittest
 import ChessBoard
 
 class ChessBoardTest(unittest.TestCase):
+    
+    #ChessBoard
     def test_ChessBoardSize(self):
         b = ChessBoard.Board()
         self.assertEqual(len(b.squares),64)
+        
+    #Piece
     def test_PieceColor(self):
         b = ChessBoard.Piece()
         self.assertFalse(b.color is None)
-    
+        
 if __name__ == '__main__':
     unittest.main()
