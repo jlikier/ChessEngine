1ed7a541c5696721399521af1d52d3684840f5c4
jlikier <jlikier@gmail.com>
2010-09-04 16:12:53 -0600

    fixed for new-style classes and added x,y to BoardSquare

--- src/ChessEngine/ChessBoard.py
+++ src/ChessEngine/ChessBoard.py
@@ -1,17 +1,37 @@
-class Board():
+class Board(object):
     squares = [0]*64
 
-class Piece():
+class Piece(object):
     COLOR_WHITE = 1
     COLOR_BLACK = 2
     COLOR_INVALID = 3
     color = COLOR_WHITE
     
 
-class BoardSquare():
+class BoardSquare(object):
     def isOccupied(self):
         return isinstance(self.piece, Piece)
         
     def __init__(self):
         self.color = Piece.COLOR_INVALID
         self.piece = None
+        self._x = None
+        self._y = None
+        
+    def get_x(self):
+        return self._x
+    
+    def set_x(self,val):
+        if val > 0 and val < 9:
+            self._x = val
+            
+    x = property(get_x,set_x)
+            
+    def get_y(self):
+        return self._y
+    
+    def set_y(self,val):
+        if val > 0 and val < 9:
+            self._y = val
+            
+    y = property(get_y,set_y)
