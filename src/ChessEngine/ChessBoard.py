5ae2f4f2c58a8d0a35dc4c886f0a9cde181732b9
Veillance <mdecour@gmail.com>
2010-09-04 13:40:26 -0500

    Changed yuh Files

--- src/ChessEngine/ChessBoard.py
+++ src/ChessEngine/ChessBoard.py
@@ -5,6 +5,15 @@ class Piece():
     COLOR_WHITE = 1
     COLOR_BLACK = 2
     COLOR_INVALID = 3
+    color = COLOR_WHITE
     
+
+class BoardSquare():
+    def isOccupied(self):
+        if isinstance(self.piece, Piece):
+            return True
+    	else: 
+            return False
     def __init__(self):
         self.color = Piece.COLOR_INVALID
+	self.piece = None
--- src/ChessEngine/UnitTest.py
+++ src/ChessEngine/UnitTest.py
@@ -12,11 +12,38 @@ class ChessBoardTest(unittest.TestCase):
     def test_BoardSquarePiece(self):
         s = ChessBoard.BoardSquare()
         self.assertTrue(hasattr(s,"piece"), "BoardSquare is missing attribute: piece")
-        
+    
+#THE BALL!!!1#
+    def test_BoardSquareLocation(self):
+        s = ChessBoard.BoardSquare()     
+	self.assertTrue(hasattr(s,"x"), "BoardSquare is missing attribute: x")
+	self.assertTrue(hasattr(s,"y"), "BoardSquare is missing attribute: y")
+	self.assertTrue(s.x is None, "BoardSquare x should be None by default")
+	self.assertTrue(s.y is None, "BoardSquare y should be None by default")
+	s.setX(9)
+	self.assertTrue(s.x is None, "BoardSquare setter should not allow illegal locations.")
+	s.setX(0)
+	self.assertTrue(s.x is None, "BoardSquare setter should not allow illegal locations.")
+	s.setX(1)
+	self.assertTrue(s.x == 1, "BoardSquare setter should allow legal locations.")
+	s.setX(100)
+	self.assertTrue(s.x is None, "BoardSquare setter should not allow illegal locations.")
+	s.setY(9)
+	self.assertTrue(s.y is None, "BoardSquare setter should not allow illegal locations.")
+	s.setY(0)
+	self.assertTrue(s.y is None, "BoardSquare setter should not allow illegal locations.")
+	s.setY(1)
+	self.assertTrue(s.y == 1, "BoardSquare setter should allow legal locations.")
+	s.setY(100)
+	self.assertTrue(s.y is None, "BoardSquare setter should not allow illegal locations.")
+#END OF THE BALL!!!1#
     def test_BoardSquareOccupied(self):
         s = ChessBoard.BoardSquare()
-        self.assertIsNone(s.piece, "BoardSquare piece should be None by default")
-        self.assertTrue(hasattr(s,"IsOccupied"), "The isOccupied function does not exist")
+	# Temporarily removing this and inserting a replacement test that works with pythons under 2.7
+        #self.assertIsNone(s.piece, "BoardSquare piece should be None by default") 
+	# This is the replacement
+	self.assertTrue(s.piece is None, "BoardSquare piece should be None by default")
+        self.assertTrue(hasattr(s,"isOccupied"), "The isOccupied function does not exist")
         self.assertTrue(not s.isOccupied(), "The BoardSquare should be empty, but isOccupied returned true")
         s.piece = ChessBoard.Piece()
         self.assertTrue(s.isOccupied(), "The BoardSquare should be occupied, but isOccupied returned false")
