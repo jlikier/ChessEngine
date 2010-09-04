6396bc138a1fe8e77f08860c9bae5af0df34d0f5
James Likier <jlikier@gmail.com>
2010-09-03 20:08:30 -0600

    Added next round of test cases

--- src/ChessEngine/UnitTest.py
+++ src/ChessEngine/UnitTest.py
@@ -8,6 +8,19 @@ class ChessBoardTest(unittest.TestCase):
         b = ChessBoard.Board()
         self.assertEqual(len(b.squares),64)
         
+    #BoardSquare
+    def test_BoardSquarePiece(self):
+        s = ChessBoard.BoardSquare()
+        self.assertTrue(hasattr(s,"piece"), "BoardSquare is missing attribute: piece")
+        
+    def test_BoardSquareOccupied(self):
+        s = ChessBoard.BoardSquare()
+        self.assertIsNone(s.piece, "BoardSquare piece should be None by default")
+        self.assertTrue(hasattr(s,"IsOccupied"), "The isOccupied function does not exist")
+        self.assertTrue(not s.isOccupied(), "The BoardSquare should be empty, but isOccupied returned true")
+        s.piece = ChessBoard.Piece()
+        self.assertTrue(s.isOccupied(), "The BoardSquare should be occupied, but isOccupied returned false")
+        
     #Piece
     def test_PieceColor(self):
         b = ChessBoard.Piece()
